6168890c3f82a1ebe433a7d461fc7f58d21ef939
ArchieMeng <archiemeng@protonmail.com>
2025-06-05 20:32:30 +0800

    Include Lindroid vendor

--- device.mk
+++ device.mk
@@ -63,3 +63,4 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/motorola/guam/guam-vendor.mk)
+$(call inherit-product, vendor/lindroid/lindroid.mk)
